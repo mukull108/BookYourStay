389588a5cecbeac055e98798f33bc8fad4a530fe
mukullFromMac <mukulsavita0108@gmail.com>
2025-09-16 16:54:46 +0530

    handled security related exceptions

--- src/main/java/com/project/HotelBookingApp/advice/GlobalExceptionHandler.java
+++ src/main/java/com/project/HotelBookingApp/advice/GlobalExceptionHandler.java
@@ -18,6 +18,37 @@ public class GlobalExceptionHandler {
         return buildApiErrorResponseEntity(apiError);
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleSpringSecurityAuthenticationExceptions(AuthenticationException exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    //this will not work directly from here,for this we will have to pass exception from filter chain context to servlet,
+    // global exception handler will only handle exceptions on servlet context
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    //when we do not have access, if you are not using this then your request will take you to the login page, after
+    //adding this you now get forbidden error because you do not have access.
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.FORBIDDEN)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception) {
         ApiError apiError = ApiError.builder()
